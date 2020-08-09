2d3e900ffe85c9c4916eb7b7e71d63948a9986c3
jshresth <shresthajyoti79@gmail.com>
2020-08-09 13:46:14 -0400

    Updates

---
+++ precache-manifest.73bff93d770e4326132038188b3bf077.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83eaa7eb90b2c0082c6f97cb248b142a",
+    "url": "/jyoti-shrestha/index.html"
+  },
+  {
+    "revision": "20553f519350e86f2dae",
+    "url": "/jyoti-shrestha/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8ef774d1f4a7bcb2b505",
+    "url": "/jyoti-shrestha/static/js/2.ebeb2c73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jyoti-shrestha/static/js/2.ebeb2c73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20553f519350e86f2dae",
+    "url": "/jyoti-shrestha/static/js/main.5514d3c2.chunk.js"
+  },
+  {
+    "revision": "35d3aa3bb5344a52e0d4",
+    "url": "/jyoti-shrestha/static/js/runtime-main.a308a8bc.js"
+  }
+]);
